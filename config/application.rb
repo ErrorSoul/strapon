175f79264afd9f1c9c3e1b07435d928eb0366a60
Abu Sultanov <neurofag@gmail.com>
2015-03-09 03:41:15 +0300

    delete vendor

--- config/application.rb
+++ config/application.rb
@@ -21,7 +21,7 @@ module Strapon
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de 
-    config.assets.paths << Rails.root.join('vendor', 'assets', 'bower_components')
+    #config.assets.paths << Rails.root.join('vendor', 'assets', 'bower_components')
     config.assets.paths << Rails.root.join("app", "assets", "images")
     config.assets.paths << Rails.root.join("app", "assets", "fonts")
     config.assets.precompile += ['application.css','admin.js', 'admins.css']
